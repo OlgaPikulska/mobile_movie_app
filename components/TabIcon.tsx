071223776036e1e54e80e0ed53b70b36228e147e
Olga Pikulska <olga.pikulska.dev@gmail.com>
2025-05-05 23:03:38 +0200

    missing tailwind configuration

--- components/TabIcon.tsx
+++ components/TabIcon.tsx
@@ -27,11 +27,9 @@ export const TabIcon = ({ focused, icon, title }: TabIconInterface) => {
       </ImageBackground>
     );
   }
+
   return (
-    <View
-      className="size-full justify-center items-center mt-4
-    rounded-full"
-    >
+    <View className="size-full justify-center items-center mt-4 rounded-full">
       <Image source={icon} tintColor="#A8B5DB" className="size-5" />
     </View>
   );
